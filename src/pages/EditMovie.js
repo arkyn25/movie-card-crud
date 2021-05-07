8ccf2f190d2253dfbec1295fa01fd83c6fbc7d02
Arlen Freitas <arlenfreitas1@gmail.com>
2021-05-07 16:58:52 -0300

    req 5 feito

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: '',
+      status: 'loading',
+      shouldRedirect: false,
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    this.setState({ shouldRedirect: true });
+  }
+
+  async fetchMovie() {
+    const { match } = this.props;
+    const movie = await movieAPI.getMovie(match.params.id);
+
+    this.setState({ movie, status: 'loaded' });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
 
     return (
@@ -30,5 +46,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default EditMovie;
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,78 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor(props) {
+    super(props);
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.fetchMovie = this.fetchMovie.bind(this);
+
+    this.state = {
+      title: '',
+      subtitle: '',
+      storyline: '',
+      genre: '',
+      rating: '',
+      imagePath: '',
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async fetchMovie() {
+    const { match } = this.props;
+    const { params } = match;
+    const movie = await movieAPI.getMovie(params.id);
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
+
+    this.setState({
+      id,
+      title,
+      subtitle,
+      storyline,
+      imagePath,
+      genre,
+      rating,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { loading } = this.state;
+    if (loading === true) return <Loading />;
+
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = this.state;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <div>
+          <Link to="/">VOLTAR</Link>
+          <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        </div>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default MovieDetails;
