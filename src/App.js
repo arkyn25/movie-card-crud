67e40001b682fd7e7185a7d2d014ff1393c30626
Arlen Freitas <arlenfreitas1@gmail.com>
2021-05-07 15:17:45 -0300

    req 1 feito

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages/index';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+        <Route path="movies:id" render={ (props) => <MovieDetails { ...props } /> } />
+        <Route component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
