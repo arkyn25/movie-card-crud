27a8b370dd93270c3c12d34f8f5ed4b5caa69387
Arlen Freitas <arlenfreitas1@gmail.com>
2021-05-07 19:00:24 -0300

    req 6 feita

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
@@ -35,6 +35,7 @@ class MovieList extends Component {
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
